f867aaf98158eab98d70144df6276bd99456de59
Jason Benn <jasoncbenn@gmail.com>
2017-09-15 14:10:13 -0700

    Finish pseudocode for schema, to_md.

--- src/db.py
+++ src/db.py
@@ -33,9 +33,9 @@ class DB:
     ''')
 
   def all(self, table):
-    return self.cursor.execute('''SELECT * FROM ${table};''', { "table": table }).fetchall()
+    return self.cursor.execute("SELECT * FROM %s;", (table,)).fetchall()
 
-  def all_google_search_results(self, table):
+  def all_nips_papers_missing_abstracts(self, table):
     return self.cursor.execute('''
       SELECT * FROM nips_papers
       LEFT JOIN abstracts ON abstracts.nips_paper_id=nips_papers.id
@@ -48,33 +48,32 @@ class DB:
     for record in records:
       keys = ", ".join(record.keys())
       values = ", ".join(record.values())
-      self.cursor.execute('INSERT INTO %s ("%s") VALUES (?);' % (table, keys), (values,))
+      self.cursor.execute("INSERT INTO %(table) ($(keys)) VALUES (%(values));", { "table": table, "keys": keys, "values": values })
     self.conn.commit()
 
-  def upsert_search_result(self, table, record):
+  def upsert_search_result(self, table, search_result):
     self.cursor.execute('''
       INSERT INTO google_search_results (nips_paper_id, abstract_url, pdf_url, fetch_attempts)
-      VALUES (${nips_paper_id}, ${abstract_url}, ${pdf_url}, 1)
+      VALUES (%(nips_paper_id), %(abstract_url), %(pdf_url), 1)
       ON CONFLICT UPDATE (fetch_attempts)
       VALUES (
-        (SELECT fetch_attempts from google_search_results WHERE nips_paper_id = ${nips_paper_id}) + 1
+        (SELECT fetch_attempts from google_search_results WHERE nips_paper_id = %(nips_paper_id)) + 1
       )
-    ''', record)
+    ''', search_result)
 
   def to_md(self, filename):
-    print "to_md"
-    # with io.open(filename, encoding="utf8") as f:
-      # from IPython import embed; embed()
-      # f.write(u"### #%s: %s\n" % (i + start_index, title))
-      # # extract, transform
-      # abstract = arxiv_url_to_abstract(arxiv_url)
-      # # load
-      # f.write(u"[%s](%s)\n" % (arxiv_url, arxiv_url))
-      # f.write(abstract + u"\n\n")
-      # f.flush()
+    print "dumping db to md"
 
-      # try:
-      #   assert title[:40] in result["title"]  # Long titles are truncated in results
-      # except AssertionError:
-      #   f.write(u"Not found on arxiv\n\n")
-      #   f.flush()
+    abstracts = self.cursor.execute('''
+      SELECT * FROM nips_papers
+      LEFT JOIN abstracts ON abstracts.nips_paper_id=nips_papers.id
+    ''').fetchall()
+
+    print "found %i abstracts" % len(abstracts)
+
+    with io.open(filename, encoding="utf8") as f:
+      for a in abstracts:
+        f.write(u"### #%s: %s\n" % (a["nips_papers.id"], a["title"]))
+        f.write(u"[Abstract](%s), [PDF](%s)\n" % (a["abstract_url"], a["pdf_url"]))
+        f.write(u"Authors: %s" % a["authors"])
+        f.write(a["abstract"] + u"\n\n")
