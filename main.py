65ab2470d443b7be576bf681eb60facf9dbf0b8b
Jason Benn <jasoncbenn@gmail.com>
2017-09-15 14:09:39 -0700

    Add print statements, correct some logic.

--- main.py
+++ main.py
@@ -11,29 +11,38 @@ from src.google_etl import GoogleETL
 from src.nips_etl import NipsETL
 
 
+NUM_NIPS_17_PAPERS = 680
+
 def scrape(start_index):
   db = DB()
   nips = NipsETL(db)
   google = GoogleETL(db)
   arxiv = ArxivETL(db)
 
-  titles = db.all("nips_papers")
-  if not len(titles):
+  titles = db.all('nips_papers')
+  print "found %s nips_papers" % len(titles)
+  if len(titles) < NUM_NIPS_17_PAPERS:
+    print "fetching..."
     response = nips.extract()
     titles = nips.transform(response)
     nips.load(titles)
 
-  for i, title in titles[start_index:]:
+  all_nips_papers_missing_abstracts = db.all_nips_papers_missing_abstracts()
+  print "found %i nips papers missing abstracts" % len(all_nips_papers_missing_abstracts)
+
+  for i, title in all_nips_papers_missing_abstracts[start_index:]:
+    print "fetching #%i: %s" % (i, title)
     response = google.extract(title)
     search_result = google.transform(response)
+    google.load(i, search_result)
+
     if search_result:
-      google.load(i, search_result)
+      print "found search result!"
       response = arxiv.extract(search_result["abstract_url"])
       abstract = arxiv.transform(response)
       arxiv.load(i, abstract)
 
   db.to_md("abstracts.md")
-
 
 if __name__ == '__main__':
   parser = ArgumentParser()
