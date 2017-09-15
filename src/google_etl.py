be381430f9165a6429e3b420abc88139038d4400
Jason Benn <jasoncbenn@gmail.com>
2017-09-15 14:10:33 -0700

    Improve google_etl#transform.

--- src/google_etl.py
+++ src/google_etl.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from .utils import clean_text
+from .utils import ascii_alphafy
 
 
 class GoogleETL:
@@ -10,11 +10,12 @@ class GoogleETL:
     self.search_engine_id = open('keys/search_engine_id').read().strip()
 
   def extract(self, title):
-    urlified_title = "+".join(clean_text(title).split(" "))
+    urlified_title = "+".join(ascii_alphafy(title).split(" "))
     url = "https://www.googleapis.com/customsearch/v1?q={}&cx={}&key={}".format(urlified_title, self.search_engine_id, self.google_search_api_key)
     print "requesting %s" % url
     response = requests.get(url)
     if response.status_code >= 400:
+      print response.status_code
       print response.text
       from IPython import embed; embed()
     return response
@@ -23,18 +24,18 @@ class GoogleETL:
     body = json.loads(response.text)
     results = body.get('items')
     first_result = results[0]
-    search_query = body['queries']['request'][0]['searchTerms']
-    truncated_search_query = search_query[:40]  # Long titles are truncated in results
-    if truncated_search_query in clean_text(first_result["title"]):
+    truncated_search_query = body['queries']['request'][0]['searchTerms'][:40]  # Long titles are truncated in results
+    if truncated_search_query in ascii_alphafy(first_result["title"]):
       url = first_result["link"]
-      result = {}
+      assert "arxiv.org" in url
+      matching_result = {}
       if "pdf" in url:
-        result["pdf_url"] = url
-        result["abstract_url"] = url.replace("pdf", "abs")
+        matching_result["pdf_url"] = url
+        matching_result["abstract_url"] = url.replace("pdf", "abs")
       else:
-        result["abstract_url"] = url
-        result["pdf_url"] = url.replace("abs", "pdf")
-      return result
+        matching_result["abstract_url"] = url
+        matching_result["pdf_url"] = url.replace("abs", "pdf")
+      return matching_result
 
   def load(self, nips_paper_id, record):
     record["nips_paper_id"] = str(nips_paper_id)
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,5 @@
-def clean_text(string):
+def ascii_alphafy(string):
   return string.replace(r':', '').replace(',', '').encode('ascii', errors='ignore')
+
+def strip_text(string):
+  return string.replace('\n', ' ').strip().encode("utf8")
